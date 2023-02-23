ce583484ad812c81466499fdfb22f5e74280b307
ilyagpp <acidbrother@yandex.ru>
2023-02-23 14:34:45 +0300

    HW2

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,50 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.ValidationUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.List;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
 
+    private final MealRepository repository;
+
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public List<Meal> getAll() {
+        return (List<Meal>) repository.getAll();
+    }
+
+    public Meal findById(int id) {
+        Meal meal = repository.get(id, SecurityUtil.authUserId());
+        ValidationUtil.checkNotFound(SecurityUtil.authUserId()==meal.getUserId(), "meal does not belong to this user");
+        return meal;
+
+    }
+
+    public boolean deleteById(int id) {
+        Meal  meal = repository.get(id, SecurityUtil.authUserId());
+
+        ValidationUtil.checkNotFound(SecurityUtil.authUserId()==meal.getUserId(), "meal does not belong to this user");
+
+        return repository.delete(id, SecurityUtil.authUserId());
+    }
+
+    public Meal save(Meal meal) {
+
+        if (!meal.isNew()){
+            ValidationUtil.checkNotFound(SecurityUtil.authUserId()==meal.getUserId(), "meal does not belong to this user");
+        }
+
+        return repository.save(meal, SecurityUtil.authUserId());
+
+    }
 }
